79c0a861cc62c72a8d0b125cbf913af301b7b72d
Retrofitt <rafaelmendozajr94@gmail.com>
2021-10-19 19:24:48 -0700

    cleaning up files

--- api/server.js
+++ api/server.js
@@ -18,24 +18,6 @@ server.use('/api/auth', authRouter)
 server.use('/api/plants', restricted, plantsRouter)
 server.use('/api/users', restricted, usersRouter)
 
-// function getAllUsers() { return db('users') }
-
-// async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-  // const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  // return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-// }
-
-// server.get('/api/users', async (req, res) => {
-//   res.json(await getAllUsers())
-// })
-
-// server.post('/api/users', async (req, res) => {
-//   res.status(201).json(await insertUser(req.body))
-// })
-
 server.get('/', (req, res)=>{
   res.send(`<h2>Thanks for visiting the "Watering My Plants" API</h2>`)
 })
